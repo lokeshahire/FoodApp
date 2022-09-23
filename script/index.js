d2b8bfd6c9dd658043506076297d75fc50f12c3c
Lokesh Ahire <105935374+lokeshahire@users.noreply.github.com>
2022-09-23 15:48:34 +0530

    my food app

---
+++ script/index.js
@@ -0,0 +1,31 @@
+import navbar from "../conponent/navbar.js";
+
+document.getElementById("navbar").innerHTML = navbar();
+
+
+
+
+
+import { getData, append } from "../conponent/fetch.js"
+let id;
+
+
+const main = async () => {
+    let input = document.querySelector("#query");
+    console.log(input)
+    let name = input.value
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+    let data = await getData(url);
+    console.log(data[0])
+    console.log(data[0].strMeal)
+
+    append(data);
+};
+
+input.addEventListener("input", async () => {
+    if (id) clearTimeout(id);
+    id = setTimeout(() => {
+        main();
+
+    }, 2000);
+})
